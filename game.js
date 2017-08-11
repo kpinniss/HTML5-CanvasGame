98dc578862f32192ce222293469907d7e0c725ad
Kristofer Inniss <kristofer.inniss@codercamps.com>
2017-08-11 14:42:29 -0700

    player movement

--- game.js
+++ game.js
@@ -6,27 +6,8 @@ class GameArea{
         this.canvas.style.backgroundColor = color;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        
     }
 
-    clear(){
-        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
-    }
-
-    playerController(player){
-    if (event.keyCode == 37 || event.keyCode == 65) {
-        player.x -= 10;
-    }
-    else if (event.keyCode == 38 || event.keyCode == 87) {
-        player.y -= 10;
-    }
-    else if (event.keyCode == 39 || event.keyCode == 68) {
-        player.x += 10;
-    }
-    else if (event.keyCode == 40 || event.keyCode == 83) {
-        player.y += 10;
-    }
-    }
 }
 
 class GameObject{
@@ -42,24 +23,50 @@ class GameObject{
     this.ctx.fillRect(this.x, this.y, this.width, this.height);
 }
 
-    update(){
-        this.ctx = this.gameArea.context;
-        this.ctx.fillStyle = this.color;
-        this.ctx.fillRect(this.x, this.y, this.width, this.height);    
+}
+
+class Game{
+    constructor(gameArea, gameobject){
+        this.gameArea = gameArea;
+        this.gameobject = gameobject;
+        console.log(this.gameobject);
+        setInterval((update)=>{this.update();}, 20);
+        document.addEventListener('keydown', (event)=>{
+        this.playerController();
+        });
     }
 
+    update(){
+        this.gameArea.context.clearRect(0, 0, this.gameArea.canvas.width, this.gameArea.canvas.height);
+        this.gameobject.ctx = this.gameArea.context;
+        this.gameobject.ctx.fillStyle = this.color;
+        this.gameobject.ctx.fillRect(this.gameobject.x, this.gameobject.y, this.gameobject.width, this.gameobject.height);
+        
+    }
+
+    playerController(){
+    if (event.keyCode == 37 || event.keyCode == 65) {
+        this.gameobject.x -= 10;
+        console.log(this.gameobject.x);
+    }
+    else if (event.keyCode == 38 || event.keyCode == 87) {
+        this.gameobject.y -= 10;
+    }
+    else if (event.keyCode == 39 || event.keyCode == 68) {
+        this.gameobject.x += 10;
+    }
+    else if (event.keyCode == 40 || event.keyCode == 83) {
+        this.gameobject.y += 10;
+    }
+    }
 }
 
 
 function startGame(){
-    //create game object
-    var game = new GameArea(500,350,"lightblue",);
-    //create and modify canvas 
-    //create player object
-    var player = new GameObject(50,50,"blue",50,50,game);
-    //update game
-    //get inputs for user to move gmae obj
-    document.addEventListener('keydown',game.playerController(player));
+    
+   var gameArea = new GameArea(550, 350,"lightBlue");
+   var player = new GameObject(50,50,"blue",20,20,gameArea);
+   var game = new Game(gameArea, player);
 }
 
 
